b69dbdf86a5d6a10c0b625e83741e1bd29e6581f
Ayush Tiwari <62030895+ayush2323@users.noreply.github.com>
2024-06-15 10:21:54 +0530

    Update agent.py

--- discord/pythonProject1/agent.py
+++ discord/pythonProject1/agent.py
@@ -8,36 +8,40 @@ load_dotenv()
 
 # call the gemini models
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash",
-                             verbose=True,
+                             verbose=False,
                              temperrature=0.5,
                              google_api_key=os.getenv("GOOGLE_API_KEY"))
 
-news_researcher = Agent(
-    role="Senior Researcher",
-    goal= "Uncover ground breaking technologies in {topic}",
-    verbose=True,
-    memory=True,
-    backstory=(
-        "Driven by curiosity, you're at the forefront of"
-        "innovation, eager to explore and share knowledge that could change"
-        "the world."
-    ),
-    tools=[tool],
-    llm=llm,
-    allow_delegation=True
-)
 
-news_writer = Agent(
-    role="Writer",
-    goal='Narrate compelling tech stories about {topic}',
-    verbose=True,
-    memory=True,
-    backstory=(
-        "With a flair for simplifying complex topics, you craft"
-        "engaging narratives that captivate and educate, bringing new"
-        "discoveries to light in an accessible manner."
-    ),
-    tools=[tool],
-    llm=llm,
-    allow_delegation=False
-)
+def news_researcher(topic):
+    return Agent(
+        role="Senior Researcher",
+        goal="Uncover ground breaking technologies in {topic}",
+        verbose=False,
+        memory=True,
+        backstory=(
+            "Driven by curiosity, you're at the forefront of"
+            "innovation, eager to explore and share knowledge that could change"
+            "the world."
+        ),
+        tools=[tool],
+        llm=llm,
+        allow_delegation=True
+    )
+
+
+def news_writer(topic):
+    return Agent(
+        role="Writer",
+        goal='Narrate compelling tech stories about {topic}',
+        verbose=False,
+        memory=True,
+        backstory=(
+            "With a flair for simplifying complex topics, you craft"
+            "engaging narratives that captivate and educate, bringing new"
+            "discoveries to light in an accessible manner."
+        ),
+        tools=[tool],
+        llm=llm,
+        allow_delegation=False
+    )
