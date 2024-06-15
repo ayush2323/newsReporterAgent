e39136d8cd2aac0f5e5387ffd1bd6379391aedb3
Ayush Tiwari <62030895+ayush2323@users.noreply.github.com>
2024-06-15 10:22:31 +0530

    Update tools.py

--- discord/pythonProject1/tools.py
+++ discord/pythonProject1/tools.py
@@ -4,9 +4,17 @@ from crewai_tools import SerperDevTool, ScrapeWebsiteTool
 
 load_dotenv()
 
-os.environ['SERPER_API_KEY'] = os.getenv('SERPER_API_KEY')
 
-tool = SerperDevTool()
-docs_scrape_tool = ScrapeWebsiteTool(
-    website_url="https://docs.crewai.com/how-to/Creating-a-Crew-and-kick-it-off/"
-)
+def get_tool():
+    os.environ['SERPER_API_KEY'] = os.getenv('SERPER_API_KEY')
+    return SerperDevTool()
+
+
+def get_docs_scrape_tool():
+    return ScrapeWebsiteTool(
+        website_url="https://docs.crewai.com/how-to/Creating-a-Crew-and-kick-it-off/"
+    )
+
+
+tool = get_tool()
+docs_scrape_tool = get_docs_scrape_tool()
